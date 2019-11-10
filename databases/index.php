80322bf3c10c9b1c6c8d1a4d7c1d18b67729b6a8
Lakshmi Pravallika Kastala <pravallikkastala17@gmail.com>
2019-11-10 15:15:09 -0500

    added more php files

---
+++ databases/index.php
@@ -0,0 +1,12 @@
+<?php
+    require "sample.php"; 
+?>
+
+    <main>
+        <p> You are logged out!</p>
+        <p> You are logged in! </p>
+    </main>
+
+    <?php
+        require "footer.php";
+    ?>
---
+++ databases/signup.php
@@ -0,0 +1,23 @@
+<?php
+    require "sample.php"; 
+?>
+    <main>
+        <div class= "wrapper-main">
+            <section class = "section-default">
+                <h1>Signup</h1>
+                <form class= "form-signup" action ="includes/signup.inc.php" method = "post">
+                    <!-- <input type ="text" name= "email" placeholder="Email"> -->
+                    <!-- <input type ="text" name= "fid" placeholder="First Name"> -->
+                    <!-- <input type ="text" name= "lid" placeholder="Last Name"> -->
+                    <input type ="text" name= "uid" placeholder="Username">
+                    <input type ="text" name= "pwd" placeholder="Password">
+                    <input type ="text" name= "pwd-repeat" placeholder="Repeat Password">
+                    <button type = "submit" name =" signup-submit">Signup</button>
+                </form>
+            </section>
+        </div>
+    </main>
+
+<?php
+    require "footer.php"; 
+?>
