920038ec9dce0d8b6972dc29ea434f0fa43493a4
Lakshmi Pravallika Kastala <pravallikkastala17@gmail.com>
2019-11-10 21:29:58 -0500

    added some stuff to header.php
    deleted sample.php

--- databases/index.php
+++ databases/index.php
@@ -3,8 +3,15 @@
 ?>
 
     <main>
-        <p> You are logged out!</p>
-        <p> You are logged in! </p>
+		<?php
+    		if(isset($_SESSION['userId'])){
+    			echo '<p> You are logged out!</p>'; 
+    		}
+    		else {
+    			echo '<p> You are logged out!</p>'; 
+    		}
+    	?>
+        
     </main>
 
     <?php
