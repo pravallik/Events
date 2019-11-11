d57a3b70edc858f2eebfa6f19b4f8592421ba4b9
alli25 <alisajohn98@gmail.com>
2019-11-11 12:02:30 -0500

    index
    new changes in index

--- databases/index.php
+++ databases/index.php
@@ -3,15 +3,19 @@
 ?>
 
     <main>
-		<?php
-    		if(isset($_SESSION['userId'])){
-    			echo '<p> You are logged out!</p>'; 
-    		}
-    		else {
-    			echo '<p> You are logged out!</p>'; 
-    		}
-    	?>
-        
+        <div class = "wrapper-main">
+            <section class= "section-default"
+
+                <?php
+                    if(isset($_SESSION['userId'])){
+                        echo '<p class = "login-status"> You are logged in!</p>';
+                    }
+                    else {
+                        echo '<p class ="login-status"> You are logged out!</p>';
+                    }
+                ?>
+            </section>
+        </div>
     </main>
 
     <?php
