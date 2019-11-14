04f229227106da31eae82962b4c8c873c19a2fc5
Lakshmi Pravallika Kastala <pravallikkastala17@gmail.com>
2019-11-13 19:11:16 -0500

    added new pages create event and stuuf

---
+++ databases/CreateEvents.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <h1> Create Events </h1>
+
+</head>
+
+<body>
+
+    <p> Only RSo officers get access to this page</p>
+    <a href = index.php><button>Go back to Home Page</button></a>
+
+    
+</body>
+
+</html>
---
+++ databases/addRSO.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <h1> Add RSO's </h1>
+
+</head>
+
+<body>
+
+    <p> Only super admins get acess to this page </p>
+    <a href = index.php><button>Go back to Home Page</button></a>
+
+    
+</body>
+
+</html>
