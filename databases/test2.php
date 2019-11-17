ce445a679af5de63c9398df85f677e95429158cb
alli25 <alisajohn98@gmail.com>
2019-11-17 15:29:00 -0500

    view events changes
    events can be seen by the user

--- databases/test2.php
+++ databases/test2.php
@@ -4,11 +4,34 @@
 
     <h1> Welcome Inside </h1>
 
+<?php 
+	require 'includes/dbh.inc.php'; 
+?>
+
 </head>
 
 <body>
-
-    <p> hjhr hdg hello watf yeufj </p>
+	
+    
+	<?php 
+	$sqlview= "SELECT * FROM events;"; 
+	$sqlev= mysqli_query($conn, $sqlview); 
+	$resultChecking =mysqli_num_rows($sqlev); 
+	if($resultChecking > 0){
+		while ($row = mysqli_fetch_assoc($sqlev)){
+			echo 'Event Title: ' ; 
+			echo $row['uidEvents'] . "<br>";
+			echo 'Location : ' ; 
+			echo $row['placeEvents'] . "<br>";
+			echo 'RSO: ' ; 
+			echo $row['OidEvents'] . "<br>";
+			echo 'Comment: ' ; 
+			echo $row['summaryEvents'] . "<br>";
+			echo '<p> --------------------------------------------- </p> '; 
+			
+		}
+	}
+	?>
     <a href = index.php><button>Go back to Home Page</button></a>
 
     
