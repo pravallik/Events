ba24d321a3d67e0e272e9dc10072f5282f96de45
alli25 <alisajohn98@gmail.com>
2019-11-17 23:28:00 -0500

    joinRso
    not connecting to database

---
+++ databases/joinRSO.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <h1> Join RSO? </h1>
+
+<?php 
+	require 'includes/dbh.inc.php'; 
+?>
+
+</head>
+
+<body>
+	
+    <form class= "form-signup" action ="includes/joinRSO.inc.php" method = "post">
+	<?php 
+	$sqlr= "SELECT * FROM rsos;"; 
+	$sqlrsp= mysqli_query($conn, $sqlr); 
+	$resultChecks =mysqli_num_rows($sqlrsp); 
+	if($resultChecks > 0){
+		while ($row = mysqli_fetch_assoc($sqlrsp)){
+			?>
+			<input type="radio" name="jorso" value= " <?php echo $row['uidRSO']; ?> "> <?php echo $row['nameRSO'] ?> <br>
+			<?php
+			
+			
+		}
+	}
+	?>
+	<button type = "submit" name ="JoinRSO">Join RSO!</button><br><br>
+	</form>
+    <a href = index.php><button>Go back to Home Page</button></a>
+
+    
+</body>
+
+</html>
